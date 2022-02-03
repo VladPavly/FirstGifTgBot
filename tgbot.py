a95c782302ede9a88b9bf1769328e573cccfe2f1
Vlad Pavlyuchkov <64595338+Vlad10081@users.noreply.github.com>
2022-02-03 12:21:41 +0300

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -3,19 +3,17 @@ from pyrogram.errors import FloodWait
 from random import randint
 import json
 import time
+import logging
+from config import *
 
 import asyncio
 import aioschedule as schedule
 
-api_id = '' #Tg api id
-api_hash = '' #Tg api hash
-target = -0 #Tg group id to which to send gifs
-tech_group = -0 #Tg group id that is needed for the fast work of the bot
-count = 5 #The number of gifs that are uploaded to the static folder
 started = False
 wait = False
+logging.basicConfig(format='%(name)s - %(levelname)s: %(message)s', level=logging.INFO)
 
-app = Client("your_username", api_id, api_hash)
+app = Client(username, api_id, api_hash)
 
 file_ids = []
 
@@ -24,26 +22,31 @@ async def on_bot_get_message(Client, message):
     global ids, file_ids, wait
 
     if message.from_user == None and message.chat.id == target and not wait:
-         wait = True
-         await message.reply_animation(file_ids[randint(0, count)])
+        wait = True
+        file_id = file_ids[randint(0, count)]
+        await message.reply_animation(file_id)
+        logging.info(f'Gif {file_id} sended')
 
     if message.text == "Update MessageId":
-         ids = []
+        ids = []
 
-         for i in range(1, count+1):
-             try:
-                 a = await app.send_animation(tech_group, f'static/{i}.gif')
-                 await asyncio.sleep(8)
-                 ids.append(a.message_id)
-             except FloodWait as e:     
-                 await asyncio.sleep(e.x)
-                 a = await app.send_animation(tech_group, f'static/{i}.gif')
-                 await asyncio.sleep(8)
-                 ids.append(a.message_id)
+        for i in range(1, count+1):
+            try:
+                a = await app.send_animation(tech_group, f'static/{i}.gif')
+            except FloodWait as e:     
+                logging.info(f'Flood')
+                await asyncio.sleep(e.x)
+                a = await app.send_animation(tech_group, f'static/{i}.gif')
+            await asyncio.sleep(8)
+            ids.append(a.message_id)
+            logging.info(f'Gif {i}')
+            logging.debug(f'Message id {ids[-1]}')
 
-         file_ids = []
-         for i in await app.get_messages(tech_group, ids):
-             file_ids.append(i.animation.file_id)
+        file_ids = []
+        for i in await app.get_messages(tech_group, ids):
+            file_ids.append(i.animation.file_id)
+        logging.info('MessageId updated')
+        logging.info(file_ids)
 
 async def get_file_id():
     global ids, file_ids, started
@@ -59,7 +62,7 @@ async def true_wait():
 
 async def scheduler():
     schedule.every(7800).seconds.do(get_file_id)
-    schedule.every(300).seconds.do(true_wait)
+    schedule.every(600).seconds.do(true_wait)
 
     while True:
         await schedule.run_pending()
@@ -71,4 +74,5 @@ if __name__ == '__main__':
     loop.create_task(scheduler())
 
 started = True
+logging.info('Bot started')
 app.run()
